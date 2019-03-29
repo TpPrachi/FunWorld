d104cf2bb8a9a07f1728dc0dc01ba63221017754
Prachi Thakkar <prachi.thakkar@solutionanalysts.com>
2019-03-29 18:38:52 +0530

    first commit

--- FunWorld/ViewController.swift
+++ FunWorld/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var text:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Fun World!", message:
+            "Hello, there...!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
